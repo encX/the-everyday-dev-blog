4cd7a5dff59e61bec446be690de1af74cc2bad11
Plai <plai.rt.w@gmail.com>
2019-12-11 01:10:21 +0700

    Disable zoom

--- src/components/site-meta.tsx
+++ src/components/site-meta.tsx
@@ -3,7 +3,7 @@ import { Helmet } from 'react-helmet';
 
 const Metadata: React.FunctionComponent = () => (
 	<Helmet>
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover"/>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover, user-scalable=no"/>
 	</Helmet>
 );
 
