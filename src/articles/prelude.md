2343f2d4fbf6245ceee6bb17e5acadd1ee14d808
Plai <plai.rt.w@gmail.com>
2019-12-15 18:54:17 +0700

    Add prelude article + image , remove test articles

---
+++ src/articles/prelude.md
@@ -0,0 +1,11 @@
+---
+title: "Prelude"
+headliner: "The beginning of next chapter"  
+featured: false
+featuredImage: ../images/prelude-featured.png 
+path: "/prelude"  
+date: "2019-12-15"
+tags: ["blog-message"]
+---
+
+เคยคิดมานานแล้วว่าจะทำ blog ดีไหม ทำลักษณะไหนดี
--- src/components/article-card.tsx
+++ src/components/article-card.tsx
@@ -4,10 +4,12 @@ import Img from 'gatsby-image';
 import './article-card.scss';
 import { MarkdownMeta } from '../types/markdown-meta';
 
+const getRandomColor = (): string => '#'+(0x1000000+(Math.random())*0xffffff).toString(16).substr(1,6);
+
 const ArticleCard: React.FunctionComponent<MarkdownMeta> = ({ title, headliner, featuredImage, path }) => (
-	<article className="article-card">
+	<article className="article-card" style={{ backgroundColor: getRandomColor() }}>
 		<Link to={path}>
-			<Img alt="Featured image" fluid={featuredImage.childImageSharp.fluid}/>
+			{ featuredImage ? <Img alt="Featured image" fluid={featuredImage.childImageSharp.fluid}/> : null }
 			<div className="article-card__text">
 				<h3>{title}</h3>
 				<h4>{headliner}</h4>
